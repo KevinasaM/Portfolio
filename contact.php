e10c859a82781699afcddb937394a416b5172b17
KevinasaM <kevinsukadana910@gmail.com>
2024-08-01 12:54:17 +0700

    update icon

--- contact.php
+++ contact.php
@@ -3,12 +3,12 @@ if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $to = 'kevinsukadana@gmail.com';
+    $to = 'kevinsukadana910@gmail.com';
     $subject = $_POST['subject'];
 
     $body = "From: $name\nE-Mail: $email\nSubject: $subject\nMessage:\n$message";
 
-    $headers = 'From: your-email@gmail.com' . "\r\n" .
+    $headers = 'From: kevinsukadana910@gmail.com' . "\r\n" .
                'Reply-To: ' . $email . "\r\n" .
                'X-Mailer: PHP/' . phpversion();
 
